4164cf0f660d5d7b700e888a06a69e81f48f0722
MEI AYUNINGSARI <75767932+meiAyuning@users.noreply.github.com>
2022-04-14 07:59:37 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Judul from './component/judul';
+// import Judul2 from './component/judul2';
+// import Judul3 from './component/judul3';
+import Toko from './component/gasss';
+import './component/stylee.css';
+function App() {
+  return (
+    <>
+      <Judul nama={"Meme"}/>
+      {/* <Judul2 />
+      <Judul3 /> */}
+      <Toko nama={"Meme"}/>
+    </>
+  )
+}
+
+export default App;
